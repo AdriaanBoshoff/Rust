ad18aba280c6251d4a631d7105e8bf4f0e65597c
Iv Misticos <iv.misticos@gmail.com>
2018-03-09 18:34:07 +0200

    Update RandomWelcome.cs

--- Plugins/RandomWelcome.cs
+++ Plugins/RandomWelcome.cs
@@ -14,32 +14,47 @@ namespace Oxide.Plugins
 
         private class Configuration
         {
-            [JsonProperty(PropertyName = "Plugin Prefix (None to remove it)")]
+            [JsonProperty(PropertyName = "Префикс Плагина (Введите none для его удаления)")]
             public string Prefix = "Welcome";
             
-            [JsonProperty(PropertyName = "Welcome Messages", ObjectCreationHandling = ObjectCreationHandling.Replace)]
-            public List<string> Messages = new List<string>
+            [JsonProperty(PropertyName = "Сообщения приветствия", ObjectCreationHandling = ObjectCreationHandling.Replace)]
+            public List<string> WelcomeMessages = new List<string>
                                            {
-                                               "{player} только что подключился - glhf!",
-                                               "{player} только что подключился. Прячьте серу!",
-                                               "{player} подключился. Все сделайте вид что заняты!",
-                                               "{player} подключился. Лучше оставить свой лут дома!",
-                                               "{player} подключился. Лучше пока не выходить из дома!",
-                                               "{player} подключился. Вечерника окончена(",
-                                               "{player} пришел на нашу вечеринку.",
-                                               "{player} только что появился на сервере.",
-                                               "{player} объявился на сервере.",
-                                               "{player} показался на сервере.",
-                                               "{player} явил себя. Все на колени!",
-                                               "{player} пришел, чтобы надрать всем задницы!",
-                                               "Где же {player}? На сервере!",
-                                               "Добро пожаловать, {player}. Мы вас ждали ( ͡° ͜ʖ ͡°)",
-                                               "Ну вот и {player}, которого мы так долго ждали.",
-                                               "Добро пожаловать, {player}. Надеемся, что вы принесли с собой банку тушенки?",
-                                               "Мужайтесь! {player} только что подключился!",
-                                               "Ну все! Сушите весла! {player} подключился!",
-                                               "Это птица! Это самолет! А, не важно. Это всего лишь {player}...",
-                                               "Тссс... Тихо... {player} подключился..."
+                                               "<color=#FFC040>{player}</color> только что подключился - glhf!",
+                                               "<color=#FFC040>{player}</color> только что подключился. Прячьте серу!",
+                                               "<color=#FFC040>{player}</color> подключился. Все сделайте вид что заняты!",
+                                               "<color=#FFC040>{player}</color> подключился. Лучше оставить свой лут дома!",
+                                               "<color=#FFC040>{player}</color> подключился. Лучше пока не выходить из дома!",
+                                               "<color=#FFC040>{player}</color> подключился. Вечерника окончена(",
+                                               "<color=#FFC040>{player}</color> пришел на нашу вечеринку.",
+                                               "<color=#FFC040>{player}</color> только что появился на сервере.",
+                                               "<color=#FFC040>{player}</color> объявился на сервере.",
+                                               "<color=#FFC040>{player}</color> показался на сервере.",
+                                               "<color=#FFC040>{player}</color> явил себя. Все на колени!",
+                                               "<color=#FFC040>{player}</color> пришел, чтобы надрать всем задницы!",
+                                               "Где же <color=#FFC040>{player}</color>? На сервере!",
+                                               "Добро пожаловать, <color=#FFC040>{player}</color>. Мы вас ждали ( ͡° ͜ʖ ͡°)",
+                                               "Ну вот и <color=#FFC040>{player}</color>, которого мы так долго ждали.",
+                                               "Добро пожаловать, <color=#FFC040>{player}</color>. Надеемся, что вы принесли с собой банку тушенки?",
+                                               "Мужайтесь! <color=#FFC040>{player}</color> только что подключился!",
+                                               "Ну все! Сушите весла! <color=#FFC040>{player}</color> подключился!",
+                                               "Это птица! Это самолет! А, не важно. Это всего лишь <color=#FFC040>{player}</color>...",
+                                               "Тссс... Тихо... <color=#FFC040>{player}</color> подключился..."
+                                           };
+            
+            [JsonProperty(PropertyName = "Сообщения о выходе", ObjectCreationHandling = ObjectCreationHandling.Replace)]
+            public List<string> ExitMessages = new List<string>
+                                           {
+                                               "<color=#FFC040>{NAME}</color> ушел. А ведь так все хорошо начиналось(",
+                                               "<color=#FFC040>{NAME}</color> ушел. Можно выходить на улицу!",
+                                               "<color=#FFC040>{NAME}</color> ушел. Не вздумайте его рейдить!",
+                                               "<color=#FFC040>{NAME}</color> ушел... за подмогой!",
+                                               "<color=#FFC040>{NAME}</color> не выдержал и ушел.",
+                                               "<color=#FFC040>{NAME}</color> нас покинул.",
+                                               "<color=#FFC040>{NAME}</color> устал и пошел спать.",
+                                               "<color=#FFC040>{NAME}</color> ушел, но обещал вернуться.",
+                                               "Мама сказала, что хватит. <color=#FFC040>{NAME}</color> ушел спать.",
+                                               "Ну наконец-то <color=#FFC040>{NAME}</color> свалил с сервера."
                                            };
         }
 
@@ -69,7 +84,23 @@ namespace Oxide.Plugins
         // ReSharper disable once UnusedMember.Local
         private void OnPlayerInit(BasePlayer player)
         {
-            string msg = _config.Messages[Random.Range(0, _config.Messages.Count)].Replace("{player}", player.displayName);
+            string msg = _config.WelcomeMessages[Random.Range(0, _config.WelcomeMessages.Count)]
+                .Replace("{player}", player.displayName)
+                .Replace("{NAME}", player.displayName);
+            
+            if (_config.Prefix.ToLower().Contains("none"))
+                Server.Broadcast(msg);
+            else Server.Broadcast(msg, _config.Prefix);
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        // ReSharper disable once UnusedParameter.Local
+        private void OnPlayerDisconnected(BasePlayer player, string reason)
+        {
+            string msg = _config.ExitMessages[Random.Range(0, _config.ExitMessages.Count)]
+                .Replace("{player}", player.displayName)
+                .Replace("{NAME}", player.displayName);
+            
             if (_config.Prefix.ToLower().Contains("none"))
                 Server.Broadcast(msg);
             else Server.Broadcast(msg, _config.Prefix);
